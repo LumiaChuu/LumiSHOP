5d3ecb47aedf1d4c1aa4ef91147c0ff8b275ebbb
Dimas Ramadhan <LumiaChuu@hotmail.com>
2025-07-07 14:35:55 +0700

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,49 @@
+import lumiLatteImg from './assets/LumiLatte.png';
+import caramelMacchiatoImg from './assets/CaramelMacchiato.png';
+import americanoImg from './assets/Americano.png';
+import mochaDelightImg from './assets/MochaDelight.png';
+
+export const menuItems = [
+  {
+    name: 'Lumi Latte',
+    description: 'Perpaduan espresso kaya rasa dengan susu steam lembut.',
+    price: 'Rp 25.000',
+    image: lumiLatteImg,
+  },
+  {
+    name: 'Caramel Macchiato',
+    description: 'Susu vanila dengan espresso dan saus karamel.',
+    price: 'Rp 28.000',
+    image: caramelMacchiatoImg,
+  },
+  {
+    name: 'Americano',
+    description: 'Espresso klasik dengan tambahan air panas.',
+    price: 'Rp 20.000',
+    image: americanoImg,
+  },
+  {
+    name: 'Mocha Delight',
+    description: 'Cokelat premium bertemu espresso dan whipped cream.',
+    price: 'Rp 30.000',
+    image: mochaDelightImg,
+  },
+];
+
+export const testimonialsData = [
+  {
+    quote: 'Kopi terbaik di kota! Suasananya juga sangat nyaman untuk bekerja.',
+    name: 'Andi P.',
+    rating: 5,
+  },
+  {
+    quote: 'Lumi Latte-nya juara! Selalu jadi pilihan utama saya.',
+    name: 'Siti K.',
+    rating: 5,
+  },
+  {
+    quote: 'Tempat yang pas untuk nongkrong sore sambil menikmati kopi berkualitas.',
+    name: 'Budi S.',
+    rating: 4,
+  },
+];
